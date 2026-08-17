0df9cd4f2a440cd38eaebc13caa013ed9107844f
AnastasiaValishina <azhidenova@gmail.com>
2026-08-17 23:41:22 +0200

    win sfx

--- Corners Game/Assets/Scripts/UI scripts/WinPopup.cs
+++ Corners Game/Assets/Scripts/UI scripts/WinPopup.cs
@@ -4,9 +4,13 @@ using UnityEngine.UI;
 
 public class WinPopup : MonoBehaviour
 {
+	[SerializeField] GameObject panel;
 	[SerializeField] TextMeshProUGUI text;
 	[SerializeField] Button restartBtn;
 	[SerializeField] Button statsBtn;
+	[SerializeField] Image[] pawnImage;
+	[SerializeField] GameObject sun;
+
 
 	private void Start()
 	{
@@ -23,10 +27,11 @@ public class WinPopup : MonoBehaviour
 			if (playerWinner == 1)
 			{
 				text.text = "онаедю!";
-				AudioPlayer.Instance.PlayWinSound();
+				PlayWin();
 			}
 			else
 			{
+				sun.SetActive(false);
 				text.text = "онпюфемхе";
 			}
 		}
@@ -34,17 +39,53 @@ public class WinPopup : MonoBehaviour
 		{
 			if (playerWinner == 2)
 			{
-				text.text = "онаедю аекшу";
-				AudioPlayer.Instance.PlayWinSound();
+				text.text = "онаедю аекшу";				
 			}
 			else
 			{
-				text.text = "онаедю вепмшу";
-				AudioPlayer.Instance.PlayWinSound();
+				text.text = "онаедю вепмшу";				
 			}
+			PlayWin();
 		}
+
 	}
 
+	private void PlayWin()
+	{
+		AudioPlayer.Instance.PlayWinSound();
+		panel.transform.localScale = Vector3.zero;
+		LeanTween.scale(panel, Vector3.one, 0.8f).setEase(LeanTweenType.easeOutBack);
+		PlayPawnsDance();
+		PlaySunburstAnimation();
+	}
+
+	private void PlayPawnsDance()
+	{
+		float jumpHeight = 30f;
+		float jumpDuration = 0.25f; 
+		float delayBetweenJumps = 0.15f; 
+
+		for (int i = 0; i < pawnImage.Length; i++)
+		{
+			GameObject pawnObj = pawnImage[i].gameObject;
+
+			LeanTween.cancel(pawnObj);
+
+			float startY = pawnImage[i].rectTransform.localPosition.y;
+
+			LeanTween.moveLocalY(pawnObj, startY + jumpHeight, jumpDuration)
+				.setEase(LeanTweenType.easeOutQuad)
+				.setLoopPingPong()
+				.setRepeat(-1) 
+				.setDelay(i * delayBetweenJumps);
+		}
+	}
+	private void PlaySunburstAnimation()
+	{
+		sun.SetActive(true);
+		LeanTween.cancel(sun);
+		LeanTween.rotateAroundLocal(sun, Vector3.forward, -360f, 10f).setLoopClamp();
+	}
 	void OnRestartClicked()
 	{
 		AudioPlayer.Instance.PlayButtonClick();
@@ -56,7 +97,6 @@ public class WinPopup : MonoBehaviour
 	{
 		AudioPlayer.Instance.PlayButtonClick();
 	}
-
 
 	private void OnDestroy()
 	{

8df22f829d9010c85b0823d5442bda38350844b1
AnastasiaValishina <azhidenova@gmail.com>
2026-08-21 12:19:21 +0200

    stats ui

--- Corners Game/Assets/Scripts/UI scripts/StatsPopup.cs
+++ Corners Game/Assets/Scripts/UI scripts/StatsPopup.cs
@@ -1,16 +1,49 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using YG;
 
 public class StatsPopup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[SerializeField] TextMeshProUGUI winsEasyText;
+	[SerializeField] TextMeshProUGUI losesEasyText;
+	[SerializeField] TextMeshProUGUI winsNormalText;
+	[SerializeField] TextMeshProUGUI losesNormalText;
+	[SerializeField] TextMeshProUGUI winsHardText;
+	[SerializeField] TextMeshProUGUI losesHardText;
+	[SerializeField] Button bottonBack;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start()
+	{
+		bottonBack.onClick.AddListener(OnBackClicked);
+	}
+
+	private void OnBackClicked()
+	{
+		AudioPlayer.Instance.PlayButtonClick();
+		gameObject.SetActive(false);
+	}
+
+	private void OnEnable()
+	{
+		UpdateStats();
+	}
+
+	private void UpdateStats()
+	{
+		winsEasyText.text = YG2.saves.winsEasy.ToString();
+		losesEasyText.text = YG2.saves.losesEasy.ToString();
+
+		winsNormalText.text = YG2.saves.winsNormal.ToString();
+		losesNormalText.text = YG2.saves.losesNormal.ToString();
+
+		winsHardText.text = YG2.saves.winsHard.ToString();
+		losesHardText.text = YG2.saves.losesHard.ToString();
+	}
+
+	private void OnDestroy()
+	{
+		if (bottonBack != null)
+			bottonBack.onClick.RemoveListener(OnBackClicked);
+	}
 }
